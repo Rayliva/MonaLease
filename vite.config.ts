393a7d2aed4b31aa587f0516bd988e9c528e8eab
Raych <racheloalden@gmail.com>
2026-04-16 14:05:47 -0400

    Update config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  // App is served from a subpath on production domain.
+  base: "/monalease/",
   plugins: [react(), tailwindcss()],
 });
